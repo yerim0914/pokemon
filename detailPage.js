a86bea19f17149ec87d9f4d91600883878b5c7cd
unknown <morri0914@hanmail.net>
2023-09-27 05:08:06 +0900

    first commit

---
+++ detailPage.js
@@ -0,0 +1,73 @@
+let koreanTypeName = [];
+
+/** 포켓몬 상세 페이지 */
+async function detailPageClick(id) {
+    /* 페이지 전환 */
+    const wrapper = document.getElementById('wrapper');
+    const detail_wrapper = document.getElementById('detail_wrapper');
+    wrapper.style.display = 'none';
+    detail_wrapper.style.display = 'flex';
+    detail_wrapper.style.visibility = 'visible';
+    koreanTypeName.length = 0;
+
+    await loadPokemon(id)
+    await setData(id);
+    
+    
+}
+let spec;
+async function loadPokemon(id) {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    spec = {
+            weight: response.data.weight,
+            height: response.data.height
+    }
+    await response.data.types.map(async (type) => {
+            const typeResponse = await axios.get(type.type.url)
+            .then((typeResponse) => {
+                const koName = typeResponse.data.names.find((name) => name.language.name === 'ko').name;
+                koreanTypeName.push(koName);
+            })
+        })
+    console.log('끝')
+};
+
+async function setData(id) {
+    return new Promise(()=>{
+        const detail_wrapper = document.getElementById('detail_wrapper');
+        const name = document.getElementById('detailName');
+        const type = document.getElementById('type');
+        const image = document.getElementById('pkimg');
+        const weight = document.getElementById('weight');
+        const height = document.getElementById('height');
+        const dId = document.getElementById('dId');
+        type.innerText = ''
+        name.innerText = ''
+        // 로딩페이지 다시 구현
+        // const loading = document.createElement("div");
+        // loading.setAttribute("class", "loading_img");
+        // loading.setAttribute("id", "loading_img")
+        // const img = document.createElement("img");
+        // img.src = "../image/spinner-solid.svg"
+        // img.style.width = "400px"
+        // loading.appendChild(img);
+        // detail_wrapper.append(loading);
+        setTimeout(()=>{
+            image.src = pokemonList[id - 1].img
+            image.style.width = "400px"
+            image.style.height = "400px"
+            // const load = document.getElementById("loading_img");
+            // load.remove();
+            const des = document.getElementById('des');
+            name.innerHTML = koreanNames[id - 1];
+            for (let typeName of koreanTypeName) {
+                // type.style.background = url("../image/grass.png")
+                type.innerText += typeName + `,`;
+            }
+            dId.innerText = `No.`+ id;
+            type.innerText = type.innerText.slice(0, -1)
+            height.innerText = spec.weight;
+            weight.innerText = spec.height;
+        }, 2000);
+    })
+}
---
+++ index.js
@@ -0,0 +1,104 @@
+let pokemonList = undefined;
+let pokemonDetail = undefined;
+
+async function callList() {
+    let page = 1;
+    try {
+        while(page !== 2) {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${page}`);
+            const data = response.data.results;
+            
+            pokemonList = data.map((pokemon, index) => {
+                return {
+                    id: index + 1,
+                    name: koreanNames[index],
+                    // img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+                    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${index+1}.png`
+                }
+            })
+            page += 1;
+        }
+        createGallery();
+    } catch (error) {
+      console.log('끝')
+    }
+};
+
+const createGallery = () => {
+    const pockemonList = document.getElementById('pokemon_list');
+    for (let i = 0; i < pokemonList.length; i++ ){
+        const id = document.createElement("div");
+        id.setAttribute("class", id);
+        id.innerText = `No.${pokemonList[i].id}`;
+
+        const name = document.createElement("div");
+        name.setAttribute("class", name);
+        name.innerText = pokemonList[i].name;
+
+        const div = document.createElement("div");
+        div.setAttribute("class", "img");
+
+        const img = document.createElement("img");
+        img.src = pokemonList[i].img;
+        img.style.width = "120px"
+
+        div.style.backgroundColor = "beige"
+        div.style.margin = "30px"
+        div.style.display = "flex"
+        div.style.flexDirection = "column"
+        div.style.padding = "20px"
+        div.style.alignItems = "center"
+        
+        div.onclick = () => {
+            detailPageClick(pokemonList[i].id);
+        }
+        div.onmouseover = () => {
+            div.style.translate = "0 -20px"
+            div.style.backgroundColor = "rgb(150, 150, 125)"
+            div.style.transition = "all 1s ease-in"
+        }
+        div.onmouseleave = () => {
+            div.style.backgroundColor = "beige"
+            div.style.translate = "initial"
+            div.style.transition = "all 1s ease-in"
+        }
+
+        div.appendChild(img);
+        div.appendChild(id);
+        div.appendChild(name);
+
+        pockemonList.appendChild(div);
+    }
+}
+
+let ability = [];
+const urls = [];
+
+for (let i = 0 ; i < 20; i++) {
+    // let url = `https://pokeapi.co/api/v2/ability/${id + 1}/`
+    let url = `https://pokeapi.co/api/v2/pokemon-species/${i + 1}`;
+    urls.push(url);
+}
+
+let requests = urls.map(url => fetch(url));
+let koreanNames = [];
+let koreanTypeNames = [];
+
+    Promise.all(requests)
+    .then((responses) => Promise.all(responses.map(res => res.json())))
+    .then(results => {
+        for (let result of results) {
+            const koreanName = result.names.find((name) => name.language.name === "ko");
+            koreanNames.push(koreanName.name);
+        }
+
+        callList()
+    })
+
+const search = () => {
+
+}
+
+
+
+
